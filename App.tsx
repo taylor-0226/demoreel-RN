9b8b1bbe341f0b6b3511fe6275e4a14e3e650f15
Peter Sun <petersun@Peters-Mac-mini.local>
2024-03-30 03:16:47 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { memo, useState } from 'react'
+
+function Label({ title }: {title: string}) {
+  console.log(`Rendered: ${title}`)
+  return <Text>{title}</Text>
+}
+
+const LabelMemo = memo(Label)
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title={`Pressed ${count} times`}
+        onPress={() => {
+          setCount(count + 1)
+        }}
+      />
+      <LabelMemo title="Label with memo" />
+      <Label title="Label" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'rgb(59, 108, 212)',
+    fontSize: 42,
+    fontWeight: '100',
+    textAlign: 'center',
+  }
 });
